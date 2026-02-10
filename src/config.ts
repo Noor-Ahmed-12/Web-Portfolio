dd3e8581a364c04123da6f4e63b06182d1b6757b
Noor Ahmed <noorshaikh51027@gmail.com>
2026-02-11 04:45:12 +0500

    Initial deployment

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// src/config.ts
+export const SITE_TITLE = 'Noor Ahmed | Multidisciplinary Technologist';
+export const SITE_DESCRIPTION = 'Integrating Data, AI, and Engineering for Global Social Impact.';
+export const GENERATE_SLIDEMAP = true;
+export const VIEW_TRANSITIONS = true;
